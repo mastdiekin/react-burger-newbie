f0f4f9b6f2604aead35746d6ab64154d0cd26bc1
Dmitriy Surkov <mastdiekin@gmail.com>
2020-04-18 01:29:31 +0300

    8_23

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Aux from "../../../hoc/Auxx";
 
 const orderSummary = (props) => {
@@ -22,7 +21,13 @@ const orderSummary = (props) => {
       <h3>{props.type === "modal" ? "Your order" : "Cart"}</h3>
       <p>A delicious burger with...</p>
       <ul>{ingredientSummary}</ul>
-      {props.type === "modal" ? <p>Continue to checkout?</p> : null}
+      {props.type === "modal" ? (
+        <div className="checkout">
+          <p>Continue to checkout?</p>
+          <button>Cancel</button>
+          <button>Continue</button>
+        </div>
+      ) : null}
     </Aux>
   );
   if (props.purchasable) {
