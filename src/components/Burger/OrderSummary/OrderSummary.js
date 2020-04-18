5bab5646562b1fbb1538c2492f0c56b31512a7fd
Dmitriy Surkov <mastdiekin@gmail.com>
2020-04-18 09:11:22 +0300

    8_37

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,48 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Auxx";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span
-          style={{
-            textTransform: "capitalize",
-          }}
-        >
-          {igKey}
-        </span>
-        :{props.ingredients[igKey]}
-      </li>
-    );
-  });
-  const r = (
-    <Aux>
-      <h3>{props.type === "modal" ? "Your order" : "Cart"}</h3>
-      <p>A delicious burger with...</p>
-      <ul>{ingredientSummary}</ul>
-      {props.type === "modal" ? (
-        <div className="checkout">
-          <p>
-            <strong>Total Price: {props.price.toFixed(2)}$</strong>
-          </p>
-          <p>Continue to checkout?</p>
-          <Button clicked={props.purchaseCancelled} btnType="Danger">
-            Cancel
-          </Button>
-          <Button clicked={props.purchaseContinued} btnType="Success">
-            Continue
-          </Button>
-        </div>
-      ) : null}
-    </Aux>
-  );
-  if (props.purchasable) {
-    return r;
-  } else {
-    return null;
-  }
-};
+class OrderSummary extends Component {
+  // componentDidUpdate() {
+  //   console.log(["OrderSummary DidUpdate"]);
+  // }
 
-export default orderSummary;
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span
+              style={{
+                textTransform: "capitalize",
+              }}
+            >
+              {igKey}
+            </span>
+            :{this.props.ingredients[igKey]}
+          </li>
+        );
+      }
+    );
+    const r = (
+      <Aux>
+        <h3>{this.props.type === "modal" ? "Your order" : "Cart"}</h3>
+        <p>A delicious burger with...</p>
+        <ul>{ingredientSummary}</ul>
+        {this.props.type === "modal" ? (
+          <div className="checkout">
+            <p>
+              <strong>Total Price: {this.props.price.toFixed(2)}$</strong>
+            </p>
+            <p>Continue to checkout?</p>
+            <Button clicked={this.props.purchaseCancelled} btnType="Danger">
+              Cancel
+            </Button>
+            <Button clicked={this.props.purchaseContinued} btnType="Success">
+              Continue
+            </Button>
+          </div>
+        ) : null}
+      </Aux>
+    );
+    if (this.props.purchasable) {
+      return r;
+    } else {
+      return null;
+    }
+  }
+}
+
+export default OrderSummary;
