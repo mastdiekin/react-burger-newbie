f6fc8113ee1799394e91548679a1cdfeb9db9e71
Dmitriy Surkov <mastdiekin@gmail.com>
2020-04-18 07:32:52 +0300

    8_29

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Aux from "../../hoc/Auxx";
 import classes from "../Layout/Layout.sass";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.content}>{props.children}</main>
   </Aux>
 );
