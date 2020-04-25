8f3aae59b7fa7578540ed7b6a28dacf2450ebb27
Dmitriy Surkov <mastdiekin@gmail.com>
2020-04-25 09:48:11 +0300

    12_2

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,6 +4,23 @@ import Aux from "../Auxx/Auxx";
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //     error: null,
+    //   };
+    //   this.reqInterceptor = axios.interceptors.request.use((req) => {
+    //     this.setState({ error: null });
+    //     return req;
+    //   });
+    //   this.resInterceptor = axios.interceptors.response.use(
+    //     (res) => res,
+    //     (error) => {
+    //       this.setState({ error: error });
+    //     }
+    //   );
+    // }
+
     state = {
       error: null,
     };
