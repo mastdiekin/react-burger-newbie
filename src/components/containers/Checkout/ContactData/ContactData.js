f8be66c2d165dc3b794b2bcebf93130d456c206b
Dmitriy Surkov <mastdiekin@gmail.com>
2020-04-26 06:07:05 +0300

    13_3

--- src/components/containers/Checkout/ContactData/ContactData.js
+++ src/components/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import Button from "../../../../components/UI/Button/Button";
 import classes from "./ContactData.sass";
 import axios from "../../../../axios-orders";
 import Spinner from "../../../../components/UI/Spinner/Spinner";
+import Input from "../../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
@@ -23,6 +24,7 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      deliveryMethod: "faster than light",
       customer: {
         name: "Dmitriy",
         address: {
@@ -30,7 +32,6 @@ class ContactData extends Component {
           country: "Russia",
         },
         email: "mastdiekin@gmail.com",
-        deliveryMethod: "faster than light",
       },
     };
     axios
@@ -53,10 +54,30 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input type="text" name="name" placeholder="Your Name" />
-        <input type="text" name="email" placeholder="Your Email" />
-        <input type="text" name="street" placeholder="Street" />
-        <input type="text" name="postal" placeholder="Postal Code" />
+        <Input
+          inputtype="input"
+          type="text"
+          name="name"
+          placeholder="Your Name"
+        />
+        <Input
+          inputtype="input"
+          type="text"
+          name="email"
+          placeholder="Your Email"
+        />
+        <Input
+          inputtype="input"
+          type="text"
+          name="street"
+          placeholder="Street"
+        />
+        <Input
+          inputtype="input"
+          type="text"
+          name="postal"
+          placeholder="Postal Code"
+        />
         <Button clicked={this.orderHandler} btnType="Success">
           Order
         </Button>
