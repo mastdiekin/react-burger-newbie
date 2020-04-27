9bf89274a9cc6a1152d9fdb6f605b9c794def559
Dmitriy Surkov <mastdiekin@gmail.com>
2020-04-27 07:56:28 +0300

    13_10

--- src/components/containers/Checkout/ContactData/ContactData.js
+++ src/components/containers/Checkout/ContactData/ContactData.js
@@ -106,17 +106,17 @@ class ContactData extends Component {
   };
 
   checkValid(value, rules) {
-    let isValid = false;
+    let isValid = true;
     if (rules.required) {
-      isValid = value.trim() !== "";
+      isValid = value.trim() !== "" && isValid;
     }
 
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
 
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
 
     return isValid;
