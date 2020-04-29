bb6204dd7e9d00afb8521c8f64fae0d52a6a050d
Dmitriy Surkov <mastdiekin@gmail.com>
2020-04-29 07:39:22 +0300

    15_2

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actions from "./actions";
+
+const initState = {
+  ingredients: null,
+  totalPrice: 0,
+};
+
+const reducer = (state = initState, action) => {
+  return state;
+};
+
+export default reducer;
