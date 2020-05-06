0b709de64f2be75ca7cb6ead59abb5c78f372bbf
Dmitriy Surkov <mastdiekin@gmail.com>
2020-05-06 20:12:34 +0300

    17_13

--- src/components/containers/Checkout/Checkout.js
+++ src/components/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../Order/CheckoutSummary/CheckoutSummary";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import ContactData from "./ContactData/ContactData";
 import { connect } from "react-redux";
 
@@ -14,20 +14,23 @@ class Checkout extends Component {
   };
 
   render() {
-    console.log(this.props);
-    return (
-      <div>
-        <CheckoutSummary
-          ingredients={this.props.ings}
-          onCheckoutCanceled={this.checkoutCanceledHandler}
-          onCheckoutContinued={this.checkoutContinuedHandler}
-        />
-        <Route
-          path={this.props.match.path + "/contact-data"}
-          component={ContactData}
-        />
-      </div>
-    );
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            onCheckoutCanceled={this.checkoutCanceledHandler}
+            onCheckoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route
+            path={this.props.match.path + "/contact-data"}
+            component={ContactData}
+          />
+        </div>
+      );
+    }
+    return summary;
   }
 }
 
