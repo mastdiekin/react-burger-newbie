83f1f19aa9f5ac2aa0b72951d2a578afff599a77
Dmitriy Surkov <mastdiekin@gmail.com>
2020-05-09 16:56:28 +0300

    18_16

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import { Route, Switch } from "react-router-dom";
 import Orders from "./components/containers/Orders/Orders";
 import Auth from "./components/containers/Auth/Auth";
 import Logout from "./components/containers/Auth/Logout/Logout";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAuthoSignUp();
+  }
   render() {
     return (
       <Layout>
@@ -23,4 +28,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAuthoSignUp: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
