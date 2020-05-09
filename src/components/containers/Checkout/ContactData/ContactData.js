b314cfc5a214f7f2a84584ddee407b35bb5ccda7
Dmitriy Surkov <mastdiekin@gmail.com>
2020-05-09 14:10:47 +0300

    18_11

--- src/components/containers/Checkout/ContactData/ContactData.js
+++ src/components/containers/Checkout/ContactData/ContactData.js
@@ -96,7 +96,7 @@ class ContactData extends Component {
       orderData: formData,
     };
 
-    this.props.onOrderBurger(order);
+    this.props.onOrderBurger(order, this.props.token);
   };
 
   checkValid(value, rules) {
@@ -198,12 +198,14 @@ const mapStateToProps = (state) => {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
+    token: state.auth.token,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)),
+    onOrderBurger: (orderData, token) =>
+      dispatch(actions.purchaseBurger(orderData, token)),
   };
 };
 
